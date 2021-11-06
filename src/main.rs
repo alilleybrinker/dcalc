fac11ba81b25f1b2cc4099582807351eaf58378d
Andrew Lilley Brinker <andrewabrinker@gmail.com>
2021-11-06 07:59:07 -0700

    Reorganized code

--- src/main.rs
+++ src/main.rs
@@ -8,49 +8,66 @@ use std::str::FromStr;
 
 const EXIT_FAILURE: i32 = 1;
 
-#[derive(Default, Debug, PartialEq, Eq, Clone, Copy)]
-struct Weeks(u64);
-
-#[derive(Default, Debug, PartialEq, Eq, Clone, Copy)]
-struct Days(u64);
-
-#[derive(Default, Debug, PartialEq, Eq, Clone, Copy)]
-struct Hours(u64);
-
-#[derive(Default, Debug, PartialEq, Eq, Clone, Copy)]
-struct Minutes(u64);
-
-#[derive(Default, Debug, PartialEq, Eq, Clone, Copy)]
-struct Seconds(u64);
-
-impl Display for Weeks {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "{}w", self.0)
+fn main() {
+    if let Err(err) = run() {
+        eprintln!("error: {}", err);
+        exit(EXIT_FAILURE);
     }
 }
 
-impl Display for Days {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "{}d", self.0)
+fn run() -> Result<()> {
+    let input = env::args().skip(1).collect::<Vec<_>>();
+
+    if input.is_empty() {
+        bail!("missing input");
     }
+
+    let result = parse_and_solve(&input)?;
+    println!("{}", result);
+    Ok(())
 }
 
-impl Display for Hours {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "{}h", self.0)
-    }
+fn parse_and_solve(input: &[String]) -> Result<Duration> {
+    let equation = parse_equation(input)?;
+    solve_equation(&equation)
 }
 
-impl Display for Minutes {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "{}m", self.0)
+fn parse_equation(input: &[String]) -> Result<Equation> {
+    let mut parts = Vec::new();
+    let mut so_far = String::new();
+
+    for part in input {
+        let part = part.trim();
+
+        if is_op(first_char(part)?) {
+            parts.push(Phrase::try_from(so_far.trim().to_owned())?);
+            parts.push(Phrase::try_from(part.to_owned())?);
+            so_far = String::default();
+        } else {
+            so_far.push_str(&format!(" {}", part));
+        }
     }
+
+    parts.push(Phrase::try_from(so_far.trim().to_owned())?);
+    Ok(parts)
 }
 
-impl Display for Seconds {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "{}s", self.0)
+fn solve_equation(equation: &[Phrase]) -> Result<Duration> {
+    let mut result = 0;
+    let mut next_op = Op::Plus;
+
+    for part in equation {
+        match part {
+            Phrase::Seconds(u) => match next_op {
+                Op::Plus => result += u,
+                Op::Minus => result -= u,
+            },
+            Phrase::Plus => next_op = Op::Plus,
+            Phrase::Minus => next_op = Op::Minus,
+        }
     }
+
+    Ok(Seconds(result).into())
 }
 
 fn parse_duration(s: &str, unit: &str, suffix: &str) -> Result<u64> {
@@ -65,78 +82,44 @@ fn parse_duration(s: &str, unit: &str, suffix: &str) -> Result<u64> {
         .map_err(Into::into)
 }
 
-impl FromStr for Weeks {
-    type Err = Error;
+fn is_op(c: char) -> bool {
+    matches!(c, '+' | '-')
+}
 
-    fn from_str(s: &str) -> Result<Self> {
-        Ok(Weeks(parse_duration(s, "weeks", "w")?))
+fn first_char(s: &str) -> Result<char> {
+    s.chars()
+        .next()
+        .ok_or_else(|| anyhow!("no char in substring"))
+}
+
+#[derive(Debug)]
+enum Phrase {
+    Seconds(u64),
+    Plus,
+    Minus,
+}
+
+type Equation = Vec<Phrase>;
+
+impl TryFrom<String> for Phrase {
+    type Error = Error;
+
+    fn try_from(s: String) -> Result<Self> {
+        match s.as_ref() {
+            "+" => Ok(Phrase::Plus),
+            "-" => Ok(Phrase::Minus),
+            s => Ok(Phrase::Seconds(Duration::from_str(s)?.as_seconds().0)),
+        }
     }
 }
 
-impl FromStr for Days {
-    type Err = Error;
-
-    fn from_str(s: &str) -> Result<Self> {
-        Ok(Days(parse_duration(s, "days", "d")?))
-    }
-}
-
-impl FromStr for Hours {
-    type Err = Error;
-
-    fn from_str(s: &str) -> Result<Self> {
-        Ok(Hours(parse_duration(s, "hours", "h")?))
-    }
-}
-
-impl FromStr for Minutes {
-    type Err = Error;
-
-    fn from_str(s: &str) -> Result<Self> {
-        Ok(Minutes(parse_duration(s, "minutes", "m")?))
-    }
-}
-
-impl FromStr for Seconds {
-    type Err = Error;
-
-    fn from_str(s: &str) -> Result<Self> {
-        Ok(Seconds(parse_duration(s, "seconds", "s")?))
-    }
+enum Op {
+    Plus,
+    Minus,
 }
 
 trait Conversion<Unit, To> {
     fn conversion_factor() -> Unit;
-}
-
-impl Conversion<u64, Seconds> for Seconds {
-    fn conversion_factor() -> u64 {
-        1
-    }
-}
-
-impl Conversion<u64, Seconds> for Minutes {
-    fn conversion_factor() -> u64 {
-        Seconds::conversion_factor() * 60
-    }
-}
-
-impl Conversion<u64, Seconds> for Hours {
-    fn conversion_factor() -> u64 {
-        Minutes::conversion_factor() * 60
-    }
-}
-
-impl Conversion<u64, Seconds> for Days {
-    fn conversion_factor() -> u64 {
-        Hours::conversion_factor() * 24
-    }
-}
-
-impl Conversion<u64, Seconds> for Weeks {
-    fn conversion_factor() -> u64 {
-        Days::conversion_factor() * 7
-    }
 }
 
 #[derive(Default, Debug, PartialEq, Eq, Clone, Copy)]
@@ -177,51 +160,6 @@ impl Display for Duration {
         }
 
         Ok(())
-    }
-}
-
-impl From<Duration> for Seconds {
-    fn from(d: Duration) -> Seconds {
-        let mut s = 0;
-
-        s += d.weeks.0 * Weeks::conversion_factor();
-        s += d.days.0 * Days::conversion_factor();
-        s += d.hours.0 * Hours::conversion_factor();
-        s += d.minutes.0 * Minutes::conversion_factor();
-        s += d.seconds.0 * Seconds::conversion_factor();
-
-        Seconds(s)
-    }
-}
-
-impl From<Seconds> for Duration {
-    fn from(s: Seconds) -> Duration {
-        let mut s = s.0;
-
-        let weeks = Weeks(s / Weeks::conversion_factor());
-        s %= Weeks::conversion_factor();
-
-        let days = Days(s / Days::conversion_factor());
-        s %= Days::conversion_factor();
-
-        let hours = Hours(s / Hours::conversion_factor());
-        s %= Hours::conversion_factor();
-
-        let minutes = Minutes(s / Minutes::conversion_factor());
-        s %= Minutes::conversion_factor();
-
-        let seconds = Seconds(s / Seconds::conversion_factor());
-        s %= Seconds::conversion_factor();
-
-        assert_eq!(s, 0);
-
-        Duration {
-            weeks,
-            days,
-            hours,
-            minutes,
-            seconds,
-        }
     }
 }
 
@@ -285,101 +223,163 @@ impl FromStr for Duration {
     }
 }
 
-fn is_op(c: char) -> bool {
-    matches!(c, '+' | '-')
-}
+impl From<Seconds> for Duration {
+    fn from(s: Seconds) -> Duration {
+        let mut s = s.0;
 
-fn first_char(s: &str) -> Result<char> {
-    s.chars()
-        .next()
-        .ok_or_else(|| anyhow!("no char in substring"))
-}
+        let weeks = Weeks(s / Weeks::conversion_factor());
+        s %= Weeks::conversion_factor();
 
-#[derive(Debug)]
-enum Phrase {
-    Seconds(u64),
-    Plus,
-    Minus,
-}
+        let days = Days(s / Days::conversion_factor());
+        s %= Days::conversion_factor();
 
-type Equation = Vec<Phrase>;
+        let hours = Hours(s / Hours::conversion_factor());
+        s %= Hours::conversion_factor();
 
-impl TryFrom<String> for Phrase {
-    type Error = Error;
+        let minutes = Minutes(s / Minutes::conversion_factor());
+        s %= Minutes::conversion_factor();
 
-    fn try_from(s: String) -> Result<Self> {
-        match s.as_ref() {
-            "+" => Ok(Phrase::Plus),
-            "-" => Ok(Phrase::Minus),
-            s => Ok(Phrase::Seconds(Duration::from_str(s)?.as_seconds().0)),
+        let seconds = Seconds(s / Seconds::conversion_factor());
+        s %= Seconds::conversion_factor();
+
+        assert_eq!(s, 0);
+
+        Duration {
+            weeks,
+            days,
+            hours,
+            minutes,
+            seconds,
         }
     }
 }
 
-fn parse_equation(input: &[String]) -> Result<Equation> {
-    let mut parts = Vec::new();
-    let mut so_far = String::new();
+#[derive(Default, Debug, PartialEq, Eq, Clone, Copy)]
+struct Weeks(u64);
 
-    for part in input {
-        let part = part.trim();
-
-        if is_op(first_char(part)?) {
-            parts.push(Phrase::try_from(so_far.trim().to_owned())?);
-            parts.push(Phrase::try_from(part.to_owned())?);
-            so_far = String::default();
-        } else {
-            so_far.push_str(&format!(" {}", part));
-        }
+impl Display for Weeks {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{}w", self.0)
     }
-
-    parts.push(Phrase::try_from(so_far.trim().to_owned())?);
-    Ok(parts)
 }
 
-enum Op {
-    Plus,
-    Minus,
-}
+impl FromStr for Weeks {
+    type Err = Error;
 
-fn solve_equation(equation: &[Phrase]) -> Result<Duration> {
-    let mut result = 0;
-    let mut next_op = Op::Plus;
-
-    for part in equation {
-        match part {
-            Phrase::Seconds(u) => match next_op {
-                Op::Plus => result += u,
-                Op::Minus => result -= u,
-            },
-            Phrase::Plus => next_op = Op::Plus,
-            Phrase::Minus => next_op = Op::Minus,
-        }
+    fn from_str(s: &str) -> Result<Self> {
+        Ok(Weeks(parse_duration(s, "weeks", "w")?))
     }
-
-    Ok(Seconds(result).into())
 }
 
-fn parse_and_solve(input: &[String]) -> Result<Duration> {
-    let equation = parse_equation(input)?;
-    solve_equation(&equation)
-}
-
-fn run() -> Result<()> {
-    let input = env::args().skip(1).collect::<Vec<_>>();
-
-    if input.is_empty() {
-        bail!("missing input");
+impl Conversion<u64, Seconds> for Weeks {
+    fn conversion_factor() -> u64 {
+        Days::conversion_factor() * 7
     }
-
-    let result = parse_and_solve(&input)?;
-    println!("{}", result);
-    Ok(())
 }
 
-fn main() {
-    if let Err(err) = run() {
-        eprintln!("error: {}", err);
-        exit(EXIT_FAILURE);
+#[derive(Default, Debug, PartialEq, Eq, Clone, Copy)]
+struct Days(u64);
+
+impl Display for Days {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{}d", self.0)
+    }
+}
+
+impl FromStr for Days {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        Ok(Days(parse_duration(s, "days", "d")?))
+    }
+}
+
+impl Conversion<u64, Seconds> for Days {
+    fn conversion_factor() -> u64 {
+        Hours::conversion_factor() * 24
+    }
+}
+
+#[derive(Default, Debug, PartialEq, Eq, Clone, Copy)]
+struct Hours(u64);
+
+impl Display for Hours {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{}h", self.0)
+    }
+}
+
+impl FromStr for Hours {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        Ok(Hours(parse_duration(s, "hours", "h")?))
+    }
+}
+
+impl Conversion<u64, Seconds> for Hours {
+    fn conversion_factor() -> u64 {
+        Minutes::conversion_factor() * 60
+    }
+}
+
+#[derive(Default, Debug, PartialEq, Eq, Clone, Copy)]
+struct Minutes(u64);
+
+impl FromStr for Minutes {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        Ok(Minutes(parse_duration(s, "minutes", "m")?))
+    }
+}
+
+impl Display for Minutes {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{}m", self.0)
+    }
+}
+
+impl Conversion<u64, Seconds> for Minutes {
+    fn conversion_factor() -> u64 {
+        Seconds::conversion_factor() * 60
+    }
+}
+
+#[derive(Default, Debug, PartialEq, Eq, Clone, Copy)]
+struct Seconds(u64);
+
+impl Display for Seconds {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{}s", self.0)
+    }
+}
+
+impl FromStr for Seconds {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        Ok(Seconds(parse_duration(s, "seconds", "s")?))
+    }
+}
+
+impl Conversion<u64, Seconds> for Seconds {
+    fn conversion_factor() -> u64 {
+        1
+    }
+}
+
+impl From<Duration> for Seconds {
+    fn from(d: Duration) -> Seconds {
+        let mut s = 0;
+
+        s += d.weeks.0 * Weeks::conversion_factor();
+        s += d.days.0 * Days::conversion_factor();
+        s += d.hours.0 * Hours::conversion_factor();
+        s += d.minutes.0 * Minutes::conversion_factor();
+        s += d.seconds.0 * Seconds::conversion_factor();
+
+        Seconds(s)
     }
 }
 
